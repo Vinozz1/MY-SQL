b7e78a2eae792f4881ee4111ef1dcb1ea79e966b
Vinnno <jonathanalvino14@gmail.com>
2025-01-10 12:42:56 +0700

    Adding layout etc

--- 01/main.py
+++ 01/main.py
@@ -1,10 +1,39 @@
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
+
+titleWeb = "FlaskKu"
+
+data = [
+    {
+        'author' : 'God',
+        'content' : 'MY LOVE STORY WITH JO YURI',
+        'date' : '9 Jan 2025'
+    },
+    { 
+        'author' : 'Creator',
+        'content' : 'MY LOVE STORY WITH JANG WONYOUNG',
+        'date' : '9 Jan 2022'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return f"Test Home...."
+    return render_template('home.html', titleWeb = titleWeb, data=data, usia = 20 )
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/content")
+def content():
+    return render_template('content.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
